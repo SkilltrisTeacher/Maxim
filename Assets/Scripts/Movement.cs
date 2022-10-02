e3cc5d84d1d2c6862d2d3f9f23251d800aeb66de
lstkGG <gasvn677@gmail.com>
2022-10-02 15:30:18 +0500

    прыжок и частично анимация

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float JumpForce = 10f;
+    [SerializeField] private KeyCode JumpButton = KeyCode.Space;
     private Rigidbody2D playerRigidbody;
 
     private void Awake()
@@ -16,11 +18,20 @@ public class Movement : MonoBehaviour
     {
         float playerInput = Input.GetAxis("Horizontal");
         Move(playerInput);
+        if (Input.GetKeyDown(JumpButton))
+        {
+            Jump();
+        }
     }
 
     private void Move(float direction)
     {
         playerRigidbody.velocity = new Vector2(direction * speed, playerRigidbody.velocity.y);
     }
-    
+
+    private void Jump()
+    {
+        Vector2 jumpVector = new Vector2(playerRigidbody.velocity.x, JumpForce);
+        playerRigidbody.velocity += jumpVector;
+    }
 }
